21589054d3ac54cbfab31f4078524add35ff4206
Caro Valverde <carolina.valverde255@gmail.com>
2024-02-19 18:40:57 -0300

    agrego funcionalidad a boton error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,11 +1,12 @@
 import { Button } from 'react-bootstrap';
 import error from '../../assets/error404.png'
+import { Link } from 'react-router-dom';
 const Error404 = () => {
     return (
         <section className="mainSection text-center">
         <img src={error} alt="error 404" />
-        <div>
-        <Button variant='success' >Volver al inicio</Button>
+        <div className='mb-4'>
+        <Button variant='success' as={Link} to="/">Volver al inicio</Button>
 
         </div>
     </section>
