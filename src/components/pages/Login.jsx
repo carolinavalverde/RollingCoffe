00b2e8d02d4575ae37c96cc7563aca98915932ad
Caro Valverde <carolina.valverde255@gmail.com>
2024-02-26 19:11:22 -0300

    agrego validacion del login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,30 @@
+import React from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const FormularioLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const validarEmail = (value) => {
+    return value.includes("@") || "El email debe contener un '@'";
+  };
+
+  const validarPassword = (value) => {
+    return (
+      /^(?=.*[A-Z])(?=.*\d).+$/.test(value) ||
+      "La contraseña debe contener al menos una mayúscula y un número"
+    );
+  };
+
   return (
     <section className="container mainSection">
       <div className="card">
@@ -9,46 +32,53 @@ const FormularioLogin = () => {
           <h1 className="mx-3 my-2">Login</h1>
         </div>
         <div className="card-body">
-          <Form>
-            <Form.Group
-              className="mb-3 container"
-              // controlId="formCorreoElectronicoLogin"
-            >
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3 container">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
                 className="form-control bg-body-tertiary"
-                // id="email"
                 placeholder="admin@rollingcode.com"
-                minlength="5"
-                maxlength="100"
-                required
+                {...register("email", {
+                  required: "El email es un campo obligatorio",
+                  validate: validarEmail,
+                })}
               />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
             </Form.Group>
-            <Form.Group
-              className="mb-3 container"
-              // controlId="formContraseñaLogin"
-            >
+            <Form.Group className="mb-3 container">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
                 className="form-control bg-body-tertiary"
-                // id="password"
                 placeholder="Escribe tu contraseña"
-                minlength="8"
-                maxlength="15"
-                required
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  minLength: {
+                    value: 8,
+                    message:
+                      "La contraseña debe tener como mínimo 8 caracteres",
+                  },
+                  maxLength: {
+                    value: 15,
+                    message:
+                      "La contraseña debe tener como máximo 15 caracteres",
+                  },
+                  validate: validarPassword,
+                })}
               />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <div className="container">
               <Button type="submit" variant="primary" className="mb-4">
                 Iniciar Sesión
               </Button>
             </div>
-            <Form.Group
-              className="mb-3 container"
-              // controlId="formOlvidasteTuContraseñaLogin"
-            >
+            <Form.Group className="mb-3 container">
               <Link to="/error">Olvidaste tu contraseña?</Link>
             </Form.Group>
           </Form>
